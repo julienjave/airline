1b67c499302dbe00c97380782a14379063436acb
julienjave <julienjave@hotmail.com>
2026-08-07 01:46:18 +0200

    Initial commit

--- functions.js
+++ functions.js
@@ -2,20 +2,43 @@ const kmDistance = window.utils.getKmDistance
 
 // 1. GET NEIGHBORS
 // Return an array of directly connected city names for a given city.
-const getNeighbors = cityName => {}
+const getNeighbors = cityName => {
+    const neighbors = []
+
+    for (const flight of graph.flights) {
+        if (flight[0] === cityName) neighbors.push(flight[1])
+        if (flight[1] === cityName) neighbors.push(flight[0])
+    }
+
+    return neighbors
+}
 
 // 2. MILEAGE
 // Return fuel used in liters as an integer for a distance in km, with kmPerLiter representing the distance you can travel on one liter of fuel. Round to the nearest integer.
 const kmPerLiter = 3
-const getFuelConsumptionLiters = distanceKm => {}
+const getFuelConsumptionLiters = distanceKm => {
+    return Math.round(distanceKm / kmPerLiter)
+}
 
 // 3. DIRECT FLIGHT
 // Return true if two cities have a direct flight between them.
-const hasDirectFlight = (city1, city2) => {}
+const hasDirectFlight = (city1, city2) => {
+    return getNeighbors(city1).includes(city2)
+}
 
 // 4. JUST ONE TRANSFER
 // Return true if city2 can be reached from city1 with one transfer or less.
-const hasOneTransferRoute = (city1, city2) => {}
+const hasOneTransferRoute = (city1, city2) => {
+    if (hasDirectFlight(city1, city2)) return true
+
+    const neighbors = getNeighbors(city1)
+
+    for (const neighbor of neighbors) {
+        if (hasDirectFlight(neighbor, city2)) return true
+    }
+
+    return false
+}
 
 // 5. COUNT ALL CITIES
 // Traverse from one city to count what total number of cities are reachable on the graph.
